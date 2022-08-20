b8fb39b82f38508e8cfd09768873fa0fb816b226
igorluigi2003 <igorluigi573@gmail.com>
2022-08-19 21:45:58 -0300

    Aula 19-08-22

--- index.php
+++ index.php
@@ -3,4 +3,44 @@
  /*
 
  */
+ // echo "Hello world - Meu nome é Iguito";
+  $idade = 32;
+  $nome = "Fernando";
+  $Altura = 1.77;
+  $solteiro = true;
+  //$solteiro = 1;
+  //echo $solteiro;
+
+  //echo 10 < 12;
+  CONST IDADE = 10;
+
+  echo IDADE;
+
+ /* $nota1 = 6;
+  if($nota1 >= 6)
+  {
+      echo "Aprovado";
+  }
+
+  else if($nota1 < 6 && $nota1 >= 2)
+  {
+      echo "Ainda tem chance";
+  } */
+
+  $nota1 = 8;
+  $nota2 = 10;
+  $media = $nota1+ $nota2;
+  $media = $media / 2;
+
+  if($media >= 6){
+      echo "Aprovado";
+  }
+
+  else{
+    echo "Reprovado";
+  }
+
+
+
+
 ?>
